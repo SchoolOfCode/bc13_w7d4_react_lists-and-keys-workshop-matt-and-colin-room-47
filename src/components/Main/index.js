288209f86eee0dc98795978e6f9359088cfa6c2d
matt190589 <matthew.doherty97@gmail.com>
2022-11-10 16:47:21 +0000

    Website rendering. Part 5 semi-complet. Need to do Img and highlights

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -9,7 +9,25 @@ import Post from '../Post';
 function Main() {
   const [posts, setPosts] = useState(samplePosts);
 
-  return <main id="main"></main>;
+  // {
+  //   postId, title, date, author, text, highlights, image;
+  // }
+  return <main id="main">{posts.map((post) => {
+    {/* console.log('post is:', postId);
+    console.log('title is:', title);
+    console.log('date is:', date);
+    console.log('author is:', author); */}
+    //console.log('text is:', text);
+    return (<Post key={post.postId}
+      title={post.title}
+      date={post.date}
+      author={post.author}
+      text={post.text}
+    // highlights={highlights}
+    //image={image} 
+    />
+    );
+  })}</main>
 }
 
 export default Main;
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -2,8 +2,30 @@ import React from 'react';
 
 import './index.css';
 
-function Post() {
-  return <article></article>;
+function Post({
+  postId,
+  title,
+  date,
+  author,
+  text,
+  highlights,
+  image }) {
+  return <article>
+    <h2 className='title'>{title}</h2>
+    <time className= 'date'>{date}</time>
+    <p className='author'>{author}</p>
+    <p className='text'>{text}</p>
+    {/* <li className='higlights'>{highlights}</li> */}
+    {/* <img className = 'image' src = {image.link} alt = {image.alt} /> */}
+  </article>;
 }
 
 export default Post;
+
+// postId: ,
+//     title: ,
+//     date: ,
+//     author: ',
+//     text: ,
+//     highlights:[]
+//      image: {}
